a548f954aea30e54e2be5b489edd3f6babada2dd
TerminalNavi <asd789ae@gmail.com>
2024-05-28 00:39:22 +0300

    v3
    #11 fixed

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,9 +1,10 @@
 from aiogram import Dispatcher
 
-from handlers import rand_find, start
+from handlers import rand_find, start, likes
 
 def include_routers(dp: Dispatcher):
     dp.include_routers(
+        likes.router,
         rand_find.router,
         start.router
     )
